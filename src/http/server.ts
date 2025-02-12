f5c18645b7b6ee372adfefb9e645a140de57dece
matheus.almeida <matheus.almeida@mv.com.br>
2025-02-12 15:09:31 -0300

    add graphql and apollo server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,17 @@
-console.log('Server Running...')
+import { buildSchema } from 'drizzle-graphql'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { db } from '../db'
+import { ApolloServer } from '@apollo/server'
+
+const { schema } = buildSchema(db)
+
+const server = new ApolloServer({ schema })
+
+async function startServer() {
+  const { url } = await startStandaloneServer(server)
+  console.log(`🚀 Server running at ${url}`)
+}
+
+startServer().catch(err => {
+  console.error('Error starting server:', err)
+})
