0612f718c430289b1be62dd20fbffa1984d5d1af
matheusf <matheus_falmeida@hotmail.com>
2025-02-11 22:04:12 -0300

    add biome and format the project

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,23 +1,22 @@
-import { db } from ".";
-import { supplierTable } from "./schema";
+import { db } from '.'
+import { supplierTable } from './schema'
 
-  
 async function seed() {
-    const supplier: typeof supplierTable.$inferInsert = {
-        name: 'Energisados',
-        logo: 'imagem ilustrativa',
-        state: 'Pernambuco',
-        costOfKwh: '50 R$ por Kw/h' ,
-        kwhMinLimit: 3000,
-        totalClients: 127,
-        rating: 'Regular',
-    }
+  const supplier: typeof supplierTable.$inferInsert = {
+    name: 'Energisados',
+    logo: 'imagem ilustrativa',
+    state: 'Pernambuco',
+    costOfKwh: '50 R$ por Kw/h',
+    kwhMinLimit: 3000,
+    totalClients: 127,
+    rating: 'Regular',
+  }
 
-    await db.insert(supplierTable).values(supplier)
-    console.log('New supplier created')
+  await db.insert(supplierTable).values(supplier)
+  console.log('New supplier created')
 
-    const suppliers = await db.select().from(supplierTable)
-    console.log('Getting all suppliers', suppliers)
+  const suppliers = await db.select().from(supplierTable)
+  console.log('Getting all suppliers', suppliers)
 }
 
 seed()
