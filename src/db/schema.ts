aafa9b776a4bcb9022051fab2bbe6a73fa8558ed
matheusf <matheus_falmeida@hotmail.com>
2025-02-11 21:10:28 -0300

    connect to the database and create table

---
+++ src/db/schema.ts
@@ -0,0 +1,13 @@
+import { createId } from "@paralleldrive/cuid2";
+import { pgTable, text, integer } from "drizzle-orm/pg-core";
+
+export const supplierTable = pgTable('supplier', {
+    id: text('id').primaryKey().$default(() => createId()),
+    name: text('name').notNull(),
+    logo: text('logo').notNull(),
+    state: text('state').notNull(),
+    costOfKwh: text('cost_of_kwh').notNull(),
+    kwhMinLimit: integer('kwh_min_limit').notNull(),
+    totalClients: integer('total_clients').notNull(),
+    rating: text('rating').notNull()
+})
---
+++ src/db/seed.ts
@@ -0,0 +1,23 @@
+import { db } from ".";
+import { supplierTable } from "./schema";
+
+  
+async function seed() {
+    const supplier: typeof supplierTable.$inferInsert = {
+        name: 'Energisados',
+        logo: 'imagem ilustrativa',
+        state: 'Pernambuco',
+        costOfKwh: '50 R$ por Kw/h' ,
+        kwhMinLimit: 3000,
+        totalClients: 127,
+        rating: 'Regular',
+    }
+
+    await db.insert(supplierTable).values(supplier)
+    console.log('New supplier created')
+
+    const suppliers = await db.select().from(supplierTable)
+    console.log('Getting all suppliers', suppliers)
+}
+
+seed()
